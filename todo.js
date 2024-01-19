a7c9a5128a8abb20e0171f29ba368d760be24660
liamstrauser <liamstrauser@gmail.com>
2024-01-18 22:46:09 -0500

    todo list stuff

--- todo.js
+++ todo.js
@@ -1,3 +1,7 @@
+function completedTasks() {
+    
+};
+
 document.querySelector('#push').onclick = function(){
     if(document.querySelector('#newtask input').value.length == 0){
         alert("please enter a task :)")
@@ -25,3 +29,35 @@ document.querySelector('#push').onclick = function(){
     }
   }
   
+  var input = document.querySelector('#newtask input');
+
+  input.addEventListener("keypress", function(event) {
+  
+    if (event.key === "Enter") {
+  
+      if(document.querySelector('#newtask input').value.length == 0){
+          alert("please enter a task :)")
+      }
+    
+      else{
+        console.log(document.querySelector('#newtask input').value)
+          document.querySelector('#tasks').innerHTML += `
+              <div class="task">
+                  <span id="taskname">
+                      ${document.querySelector('#newtask input').value}
+                  </span>
+                  <button class="delete">
+                    <i class="far fa-trash-alt"></i>DONE
+                </button>
+              </div>
+          `;
+          document.getElementById("txt").value = "";
+          var current_tasks = document.querySelectorAll(".delete");
+          for(var i=0; i<current_tasks.length; i++){
+              current_tasks[i].onclick = function(){
+                  this.parentNode.remove();
+              }
+          }
+      }
+    }
+  });
