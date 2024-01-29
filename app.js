c493d1338ebb3dd87610bb41c70dce20f3825d42
martinalipczyk <marlip26@bergen.org>
2024-01-29 15:36:44 -0500

    donwloaded node (ik there are lotssss of errors)

---
+++ app.js
@@ -0,0 +1,60 @@
+//set up the server
+const express = require( "express" );
+const app = express();
+const port = 3000;
+
+
+// define middleware that logs all incoming requests
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+} );
+
+// define a route for the default home page
+app.get( "/", ( req, res ) => {
+    res.sendFile( __dirname + "/views/index.html" );
+} );
+
+
+
+app.get( "/home", ( req, res ) => {
+    res.sendFile( __dirname + "/views/home.html" );
+} );
+// define a route for the assignment list page
+app.get( "/assignments", ( req, res ) => {
+    res.sendFile( __dirname + "/views/assignments.html" );
+} );
+
+// define a route for the assignment detail page
+app.get( "/assignments/detail", ( req, res ) => {
+    res.sendFile( __dirname + "/views/detail.html" );
+} );
+
+app.get( "/createaccount", ( req, res ) => {
+    res.sendFile( __dirname + "/views/createaccount.html" );
+} );
+
+app.get( "/goalsetting", ( req, res ) => {
+    res.sendFile( __dirname + "/views/goalsetting.html" );
+} );
+
+app.get( "/todo", ( req, res ) => {
+    res.sendFile( __dirname + "/views/todo.html" );
+} );
+
+app.get( "/moodtracker", ( req, res ) => {
+    res.sendFile( __dirname + "/moodPages/moodtracker.html" );
+} );
+
+app.get( "/journaling", ( req, res ) => {
+    res.sendFile( __dirname + "/views/journaling.html" );
+} );
+
+app.get( "/pictureupload", ( req, res ) => {
+    res.sendFile( __dirname + "/views/pictureupload.html" );
+} );
+
+// start the server
+app.listen( port, () => {
+    console.log(`App server listening on ${ port }. (Go to http://localhost:${ port })` );
+} );
