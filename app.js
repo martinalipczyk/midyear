a49c7ede70683551f5d6e4ccf6d6d29330868aa5
liamstrauser <liamstrauser@gmail.com>
2024-02-02 08:48:36 -0500

    wqfqwfwqfq

--- app.js
+++ app.js
@@ -52,6 +52,7 @@ app.get( "/", ( req, res ) => {
     res.render("login");
 } )
 
+
 app.get( "/home", ( req, res ) => {
     res.render('home', {user_id: userid, username: userna});
 } )
@@ -213,7 +214,7 @@ app.get('/getUserTasks', (req, res) => {
         }
     
     
-        const getUserTasksQuery = 'SELECT task_name FROM tasks WHERE user_id = ?';
+        const getUserTasksQuery = 'SELECT task_name FROMs tasks WHERE user_id = ?';
         db.query(getUserTasksQuery, [userid], (err, results) => {
             if (err) {
                 console.error('Error fetching user tasks:', err);
